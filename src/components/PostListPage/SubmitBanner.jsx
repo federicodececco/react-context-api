43da9a885336361a9399371303c5fe7c042c15f4
federicodececco <federicodececco1@gmail.com>
2025-02-11 17:06:04 +0100

    add:setTimeout to the banner

--- src/components/PostListPage/SubmitBanner.jsx
+++ src/components/PostListPage/SubmitBanner.jsx
@@ -1,15 +1,22 @@
 import { useSubmitProvider } from '../../contexts/SubmitContext'
+
 export default function SubmitBanner() {
   const { trigger, setTrigger } = useSubmitProvider()
-  if (!trigger) return
-  else
-    return (
-      <>
-        <div className='fixed bottom-10 mx-auto max-w-5xl translate-x-50 rounded-lg bg-blue-600/75 py-4'>
-          <div className='container mx-auto px-62 text-lg text-white'>
-            <h1>Staff member added with success!</h1>
-          </div>
+  console.log(trigger)
+
+  if (!trigger) {
+    return
+  } else
+    setTimeout(() => {
+      setTrigger(false)
+    }, 2000)
+  return (
+    <>
+      <div className='fixed bottom-10 mx-auto max-w-5xl translate-x-50 rounded-lg bg-blue-600/75 py-4'>
+        <div className='container mx-auto px-62 text-lg text-white'>
+          <h1>Staff member added with success!</h1>
         </div>
-      </>
-    )
+      </div>
+    </>
+  )
 }
